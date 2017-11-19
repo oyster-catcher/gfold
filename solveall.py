d0a0faac3529962c406ade8cf09f640cdb226226
Adrian Skilling <adrianskilling@Adrians-MacBook-Pro.local>
2017-11-19 16:36:44 +0000

    Run gFold for a range of durations and find the optimum, and plot

---
+++ solveall.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import numpy as np
+import simulate
+import gfold
+
+f=open("fuel.dat","w")
+dat=[]
+
+#######################################################
+# MAIN PARAMETERS
+#######################################################
+N = 20
+r0  = np.array([-12,120])
+rr0 = np.array([0.0,0.0])
+rf  = np.array([0 ,0])
+g   = np.array([0 ,-0.6])
+max_thrust = 1.2
+
+# Returns array of accelerations
+min_fuel = 9999
+min_N    = None
+for N in range(5,60,2):
+  fuel, rrr = gfold.solve(r0,rr0,N,g,max_thrust=max_thrust)
+  if rrr:
+    positions = simulate.simulate(r0,rr0,rrr,N,g,dt=1)
+    simulate.plot(positions,rrr,title="T=%d, fuel=%.1f" % (N,fuel))
+  if rrr and fuel < min_fuel:
+    min_fuel = fuel
+    min_N    = N
+  print >>f, N,fuel
+  dat.append( fuel )
+f.close()
+
+# Simulate best duration
+fuel, rrr = gfold.solve(r0,rr0,min_N,g,max_thrust=max_thrust)
+positions = simulate.simulate(r0,rr0,rrr,min_N,g,dt=1)
+
+# Plot best duration
+for i,f in enumerate(dat):
+  print "%d %.2f" % (5+i,f)
+simulate.plot(positions,rrr,title="Optimal T=%d, fuel=%.1f" % (min_N,min_fuel))
